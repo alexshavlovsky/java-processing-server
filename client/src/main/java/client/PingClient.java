da9ac71c703e87f23d183b86d37c8e4cf68e2526
alexshavlovsky <ctzn@tut.by>
2019-10-09 22:49:58 +0300

    update a ping client class

--- client/src/main/java/client/PingClient.java
+++ client/src/main/java/client/PingClient.java
@@ -1,36 +1,50 @@
 package client;
 
-import java.io.BufferedReader;
+import org.apache.log4j.Logger;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import static server.encoder.StreamEncoder.readString;
+import static server.encoder.StreamEncoder.writeString;
+
 public class PingClient implements IPingClient {
+
+    final private Logger logger;
 
     final private InetAddress address;
     final private int port;
     final private int readTimeout;
 
-    public PingClient(InetAddress address, int port, int readTimeout) {
+    public PingClient(String clientName, InetAddress address, int port, int readTimeout) {
+        this.logger = Logger.getLogger(PingClient.class + ":" + clientName);
         this.address = address;
         this.port = port;
         this.readTimeout = readTimeout;
     }
 
     @Override
-    public String sendPing(String message) {
-        try (Socket socket = new Socket(address, port);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-            out.println(message);
-            socket.setSoTimeout(readTimeout);
-            return in.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public String sendPing(String msg) {
+        if (logger.isDebugEnabled()) logger.debug("Connecting to " + address.getHostAddress() + ":" + port);
+        while (true)
+            try (Socket socket = new Socket(address, port);
+                 DataInputStream in = new DataInputStream(socket.getInputStream());
+                 DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+                if (logger.isDebugEnabled()) logger.debug("Writing to socket " + msg.length() + " bytes");
+                writeString(out, msg);
+                socket.setSoTimeout(readTimeout);
+                if (logger.isDebugEnabled()) logger.debug("Reading from socket");
+                return readString(in);
+            } catch (ConnectException e) {
+                // reconnect
+            } catch (IOException e) {
+                logger.error("Exception", e);
+                return null;
+            }
     }
 
 }
