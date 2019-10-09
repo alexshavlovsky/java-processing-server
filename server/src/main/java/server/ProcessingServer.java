8d0b5f5b2c3e2b0f956c4f440233f40e60005d5b
alexshavlovsky <ctzn@tut.by>
2019-10-09 22:49:28 +0300

    update the server class

--- server/src/main/java/server/ProcessingServer.java
+++ server/src/main/java/server/ProcessingServer.java
@@ -7,18 +7,18 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ProcessingServer extends Thread {
 
-    private final static Logger logger = Logger.getLogger(ProcessingServer.class);
+    private final Logger logger;
     private final int port;
     private final int socketTimeout;
     private final int processingPoolSize;
     private final WorkerFactory workerFactory;
 
     public ProcessingServer(String serverThreadName, int port, int clientSocketTimeout, int processingPoolSize, WorkerFactory workerFactory) {
+        this.logger = Logger.getLogger(ProcessingServer.class + ":" + serverThreadName);
         this.setName(serverThreadName);
         this.port = port;
         this.socketTimeout = clientSocketTimeout;
@@ -28,40 +28,51 @@ public class ProcessingServer extends Thread {
 
     public void run() {
 
-        logger.info(this.getName() + " initialisation...");
+        logger.info("Initialisation");
 
         ExecutorService processingPool = Executors.newFixedThreadPool(processingPoolSize);
 
         try (AutoCloseable close = processingPool::shutdown;
              ServerSocket serverSocket = new ServerSocket(port)) {
 
-            logger.info(this.getName() + " is running on port " + serverSocket.getLocalPort());
+            logger.info("Listening on port " + serverSocket.getLocalPort());
 
-            while (true) {
+            while (true) try {
+
+                serverSocket.setSoTimeout(500);
+                Socket client = serverSocket.accept();
+                if (logger.isDebugEnabled())
+                    logger.debug("Accepted connection from " + client.getInetAddress().getHostAddress());
+
+                client.setSoTimeout(socketTimeout);
+                Future<Integer> future = processingPool.submit(workerFactory.createWorker(client));
+
                 try {
-
-                    serverSocket.setSoTimeout(500);
-                    Socket client = serverSocket.accept();
-//                    if (logger.isDebugEnabled())
-
-                    client.setSoTimeout(socketTimeout);
-                    processingPool.submit(workerFactory.createWorker(client));
-
-                } catch (SocketTimeoutException e) {
-                    if (this.isInterrupted()) break;
-                } catch (IOException e) {
-                    logger.error(this.getName() + " error", e);
+                    Integer exitCode = future.get();
+                    if (!exitCode.equals(0))
+                        logger.error("Worker for " + client.getInetAddress().getHostAddress() + " finished with exit code " + exitCode);
+                } catch (CancellationException e) {
+                    logger.error("Worker cancellation exception", e);
+                } catch (ExecutionException e) {
+                    logger.error("Worker execution exception", e.getCause());
+                } catch (InterruptedException e) {
                     break;
                 }
+
+            } catch (SocketTimeoutException e) {
+                if (this.isInterrupted()) break;
+            } catch (IOException e) {
+                logger.error("Main loop exception", e);
+                break;
             }
 
-            logger.info(this.getName() + " shutting down...");
+            logger.info("Shutting down");
 
         } catch (Exception e) {
-            logger.error(this.getName() + " error", e);
+            logger.error("Initialisation exception", e);
         }
 
-        logger.info(this.getName() + " stopped");
+        logger.info("Terminated");
 
     }
 
