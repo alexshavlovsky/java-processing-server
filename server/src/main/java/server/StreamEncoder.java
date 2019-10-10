85db735af86c4cac56decfa16f37dfea079065bb
alexshavlovsky <ctzn@tut.by>
2019-10-10 16:58:19 +0300

    update the stream encoder

---
+++ server/src/main/java/server/StreamEncoder.java
@@ -0,0 +1,44 @@
+package server;
+
+import server.processingstrategy.ProcessingException;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class StreamEncoder {
+
+    public static void writeClientStatus(DataOutputStream out, ClientStatus msg) throws IOException {
+        out.writeInt(msg.getRetryConnectionCount());
+        out.flush();
+    }
+
+    public static ClientStatus readClientStatus(DataInputStream in) throws IOException {
+        return new ClientStatus(in.readInt());
+    }
+
+    public static void writeString(DataOutputStream out, String msg) throws IOException {
+        out.writeInt(msg.length());
+        out.writeBytes(msg);
+        out.flush();
+    }
+
+    public static void writeProcessingException(DataOutputStream out, ProcessingException e) throws IOException {
+        out.writeInt(e.getExitCode());
+        writeString(out, e.getMessage());
+    }
+
+    public static ProcessingException readProcessingException(DataInputStream in) throws IOException {
+        int exitCode = in.readInt();
+        if (exitCode == 0) return null;
+        return new ProcessingException(readString(in), exitCode);
+    }
+
+    public static String readString(DataInputStream in) throws IOException {
+        int msgLength = in.readInt();
+        byte[] buf = new byte[msgLength];
+        in.readFully(buf);
+        return new String(buf);
+    }
+
+}
